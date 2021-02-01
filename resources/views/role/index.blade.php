9cccc0261737df2b90959fcf4d0d7faa4fcdcbf6
alimarchal <alirazamarchal@hotmail.com>
2021-02-01 21:31:37 +0500

    Changes added log

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -35,7 +35,7 @@
     </script>
 
 
-    <div class="mt-5" style="text-align: center;">
+    <div class="mt-5" style="Ztext-align: center;">
         {{-- <a href="/dashboard"
             class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-600 transition ease-in-out duration-150">
             Back
