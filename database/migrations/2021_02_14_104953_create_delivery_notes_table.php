0bd8db07d64e2a48c1273901619864d7dccf1afe
alimarchal <alirazamarchal@hotmail.com>
2021-02-15 17:30:54 +0500

    DPO

--- database/migrations/2021_02_14_104953_create_delivery_notes_table.php
+++ database/migrations/2021_02_14_104953_create_delivery_notes_table.php
@@ -15,6 +15,19 @@ class CreateDeliveryNotesTable extends Migration
     {
         Schema::create('delivery_notes', function (Blueprint $table) {
             $table->id();
+            $table->string('business_id')->nullable();
+            $table->string('item_code')->nullable();
+            $table->string('item_name')->nullable();
+            $table->string('uom')->nullable();
+            $table->string('packing')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('quantity')->nullable();
+            $table->string('unit_price')->nullable();
+            $table->string('rfq_no')->nullable();
+            $table->string('rfq_item_no')->nullable();
+            $table->string('qoute_no')->nullable();
+            $table->string('payment_term')->nullable();
+            $table->string('delivery_address')->nullable();
             $table->timestamps();
         });
     }
