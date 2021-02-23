56f00c77dabd76602fa3c2a20a5f6a1ed0847c15
alimarchal <alirazamarchal@hotmail.com>
2021-02-23 11:32:27 +0500

    remove vendor

--- vendor/label84/laravel-logviewer/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Label84\LogViewer\Http\Controllers\LogViewerController;
-
-Route::get('/', [LogViewerController::class, 'index'])->name('logviewer.index');
