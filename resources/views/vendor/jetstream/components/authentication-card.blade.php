f0ad9e5909040b8f91d1dcb4d255d8a0ee78ceee
Ali Raza Marchal <alirazamarchal@hotmail.com>
2021-03-10 19:06:33 +0500

    changes in register

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -3,7 +3,7 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-4xl mt-6 px-10 py-10 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
