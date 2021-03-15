d08ff5b0ec73c0507a8557b2dea834c34fa0cb10
Ali Raza Marchal <alirazamarchal@hotmail.com>
2021-03-15 18:48:18 +0500

    API Changes

--- resources/views/mail/otp/password.blade.php
+++ resources/views/mail/otp/password.blade.php
@@ -1,10 +1,11 @@
 @component('mail::message')
 # One time password for delivery receiving
+Your delivery is here. Please share the OTP code with the driver after unloading the delivery.
 
 @component('mail::button', ['url' => '#'])
 OTP CODE: {{$otp}}
 @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
+Thanks you for using,<br>
+{{ config('app.name') }} Platform
 @endcomponent
