e6dfb1c3690ba9c5ac9674f3ee59baa4f3ed268c
Ali Raza Marchal <alirazamarchal@hotmail.com>
2021-04-22 19:24:19 +0500

    ire updated

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
