ee82e7a6c2557a1caeb6939449ecf78f040e5fcf
Ali Raza Marchal <alirazamarchal@hotmail.com>
2021-05-05 18:09:47 +0500

    changes in category

--- resources/views/category/category/sub_category_list.blade.php
+++ resources/views/category/category/sub_category_list.blade.php
@@ -3,7 +3,7 @@
     @if (!count($cate->subcategory))
         id: {{ $cate->id }},
     @endif
-    text: "{{ strtoupper($cate->name) }} - {{ $cate->name_ar }}",
+    text: "{{ ($cate->name != "Other"?strtoupper($cate->name): strtoupper(\App\Models\Category::where('id',$cate->parent_id)->first()->name) . ' (' . $cate->name . ') - ' . \App\Models\Category::where('id',$cate->parent_id)->first()->name_ar ) }} - {{ $cate->name_ar }}",
     @if (count($cate->subcategory))
         inc: [@include('category.category.sub_category_list',['categories' => $cate->subcategory])]
     @endif
