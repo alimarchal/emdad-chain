423155b4e2016249c346beca961f10a01240bc8e
Ali Raza Marchal <alirazamarchal@hotmail.com>
2021-06-15 20:58:09 +0500

    resolve RFQ aero conflicts

--- resources/views/RFQ/create.blade.php
+++ resources/views/RFQ/create.blade.php
@@ -321,19 +321,19 @@
                         <div style="background: #DEEAF6; min-height: 235px; padding-top: 1px;" class="flex">
 
                             <div class="left-info_holder flex-1">
-                                <div class="my-5 pl-5 ">
+                                <div class="my-5 pl-5 " style="padding-left: 40px;">
                                     <span class="font-bold color-1f3864 text-lg">RFQ Information</span>
                                     <hr style="border-top: 1px solid gray;width: 25%;">
                                 </div>
-                                <div class="my-5 pl-5 ">
+                                <div class="my-5 pl-5 "  style="padding-left: 40px;">
                                     Display Company Name: @include('misc.required')<div class="relative inline-flex ">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none" style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
                                                 fill="#000000" fill-rule="nonzero" /></svg>
                                         <select
-                                            class=" font-bold h-10 pl-5 pr-3 bg-transparent hover:border-gray-400 focus:outline-none appearance-none"
+                                            class=" font-bold h-10 pl-5 pr-3 bg-transparent hover:border-gray-400 focus:outline-none appearance-none" style="width: 8px; height: 8px;"
                                             required name="company_name_check" id="company_name_check">
                                             <option value="">Select</option>
                                             <option
@@ -346,7 +346,7 @@
                                     </div>
                                     <br>
                                     Payment Mode: @include('misc.required')<div class="relative inline-flex">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -392,7 +392,7 @@
                                         </select>
                                     </div> <br>
                                     Required Sample: @include('misc.required')<div class="relative inline-flex">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -420,7 +420,7 @@
                                 </div>
                                 <div class="my-5 pl-5 ">
                                     Warehouse Location: @include('misc.required')<div class="relative inline-flex">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -439,7 +439,7 @@
                                         </select>
                                     </div> <br>
                                     Delivery Period: @include('misc.required')<div class="relative inline-flex">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -594,7 +594,7 @@
                             </td>
                             <td>
                                 <div class="relative inline-flex">
-                                    <svg class="w-2 h-2 absolute top-0 right-1.5 mt-4 pointer-events-none"
+                                    <svg class="w-2 h-2 absolute top-0 right-1.5 mt-4 pointer-events-none" style="width: 8px; height:8px;"
                                          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                         <path
                                             d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -903,7 +903,7 @@
                                 </div>
                                 <div class="my-5 pl-5 ">
                                     Display Company Name: @include('misc.required')<div class="relative inline-flex ">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -922,7 +922,7 @@
                                     </div>
                                     <br>
                                     Payment Mode: @include('misc.required')<div class="relative inline-flex">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -968,7 +968,7 @@
                                         </select>
                                     </div> <br>
                                     Required Sample: @include('misc.required')<div class="relative inline-flex">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -997,7 +997,7 @@
                                 </div>
                                 <div class="my-5 pl-5 ">
                                     Warehouse Location: @include('misc.required')<div class="relative inline-flex">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -1016,7 +1016,7 @@
                                         </select>
                                     </div> <br>
                                     Delivery Period: @include('misc.required')<div class="relative inline-flex">
-                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"
+                                        <svg class="w-2 h-2 absolute top-0 right-0 mt-4 pointer-events-none"  style="width: 8px; height: 8px;"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                             <path
                                                 d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -1152,7 +1152,7 @@
                             </td>
                             <td>
                                 <div class="relative inline-flex">
-                                    <svg class="w-2 h-2 absolute top-0 right-1.5 mt-4 pointer-events-none"
+                                    <svg class="w-2 h-2 absolute top-0 right-1.5 mt-4 pointer-events-none" style="width: 8px; height: 8px;"
                                          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
                                         <path
                                             d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
