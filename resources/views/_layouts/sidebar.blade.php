13382b1f36de83828b8056756e87c1749b96bd71
UmairPervaiz <31058192+UmairPervaiz@users.noreply.github.com>
2021-09-07 16:21:30 +0500

    sidebar display count 0 bug removed

--- resources/views/_layouts/sidebar.blade.php
+++ resources/views/_layouts/sidebar.blade.php
@@ -644,7 +644,7 @@
                                 <path
                                     d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z"></path>
                             </svg>
-                            <a href="{{ route('singleCategoryRFQs') }}"><span class="mx-2 ">{{ __('sidebar.Single category') }} @if(count($eOrders) > 0 && count($quotesNotPresent) > 0) ({{count(array_unique($quotesNotPresent))}}) @else (0) @endif </span></a>
+                            <a href="{{ route('singleCategoryRFQs') }}"><span class="mx-2 ">{{ __('sidebar.Single category') }} @if(count($eOrders) > 0 && count($quotesNotPresent) > 0) ({{count(array_unique($quotesNotPresent))}}) @else {{ '('. 0 .')' }} @endif </span></a>
                         </li>
                     </ul>
                 </div>
