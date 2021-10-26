233f93343c1128d2efb42228e6100df6b32b719a
Ali Raza Marchal <alirazamarchal@hotmail.com>
2021-10-26 21:24:16 +0500

    api append model change

--- app/Models/DeliveryComment.php
+++ app/Models/DeliveryComment.php
@@ -9,10 +9,17 @@ class DeliveryComment extends Model
 {
     use HasFactory;
 
+    protected $appends = ['delivery_note_id'];
     protected $fillable = ['delivery_id', 'user_id', 'business_id', 'comment_content', 'comment_type', 'rating',];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getDeliveryNoteIdAttribute()
+    {
+        $delivery_note_id = Delivery::find($this->delivery_id)->delivery_note_id;
+        return $delivery_note_id;
     }
 }
