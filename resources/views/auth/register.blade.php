4b436226f84cace0ca387d50a0cffc558030450c
UmairPervaiz <31058192+UmairPervaiz@users.noreply.github.com>
2021-12-23 18:13:50 +0500

    Added home button in register views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,9 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
+            <a href="{{route('english.index')}}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150" style="border-radius: 25px;margin-top: 15px">
+                {{ __('sidebar.Home') }}
+            </a>
         </x-slot>
         <x-jet-validation-errors class="mb-4"/>
 
